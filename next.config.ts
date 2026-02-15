83e182869538de4abd2538b682d50aa0abb24c96
Taylor Mabry <taylormabry@Taylors-MacBook-Air.local>
2026-02-15 00:35:07 -0500

    Remove next-pwa (Turbopack conflict fix)

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
+const nextConfig: NextConfig = {};
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-};
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: isDev, // don't generate service worker during dev
-  register: true,
-  skipWaiting: true,
-});
-
-export default withPWA(nextConfig);
+export default nextConfig;
